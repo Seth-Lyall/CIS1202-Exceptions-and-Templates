d3472566c2d55663d309ebecc246998c1c765fa9
lyalls2004@gmail.com <lyalls2004@gmail.com>
2022-04-12 09:59:41 -0400

    Add project files.

---
+++ CIS1202 Exceptions and Templates/CIS1202 Exceptions and Templates.cpp
@@ -0,0 +1,28 @@
+// Seth Lyall
+// CIS1202 K01
+// April 12, 2022
+
+#include "PersonalLibrary.h"
+using namespace std;
+
+char character(char, int);
+
+int main() {
+	
+	try {
+
+	}
+	catch (char invalidCharacterException) {
+
+
+	}
+	catch () {
+
+	}
+
+	endProgram();
+}
+
+char character(char start, int offset) {
+
+}
